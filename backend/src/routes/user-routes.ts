4d9768b0c827a8baf9686f81eab2d1af9a22711b
theBappy <abappy575@gmail.com>
2025-08-05 21:26:08 +0600

    update user account API for name and upload profile picture via multer to cloudinary

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getCurrentUserController } from "../controllers/user-controller";
+import { getCurrentUserController, updateUserController } from "../controllers/user-controller";
+import { upload } from "../config/cloudinary-config";
 
 const userRoutes = Router();
 
 userRoutes.get("/current-user", getCurrentUserController);
+userRoutes.put("/update", upload.single("profilePicture"), updateUserController);
 
 export default userRoutes;
